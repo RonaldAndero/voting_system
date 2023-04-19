3230e3fc5fdb19977dc85733e0cdb8e7a3777da1
Ronald Andero Õisman <ronald-andero.oisman@khk.ee>
2023-04-19 16:26:25 +0300

    WIP

--- public/submit_vote.php
+++ public/submit_vote.php
@@ -24,38 +24,23 @@ if ($result->num_rows > 0) {
     // Voter exists in the database
     $row = $result->fetch_assoc();
     $voter_id = $row['id'];
-    $previous_vote_time = $row['hääletuse_aeg'];
-    $previous_vote = $row['otsus'];
 
-    // Check if the voter has already voted
-     if (is_null($previous_vote)) {
-         // Voter has not yet voted
-         $sql = "UPDATE HAALETUS SET otsus=$vote, hääletuse_aeg=CURRENT_TIMESTAMP WHERE id=$voter_id";
-         if ($conn->query($sql) === TRUE) {
-             echo "Vote registered successfully!";
-         } else {
-             echo "Error: " . $sql . "<br>" . $conn->error;
-         }
-     } else {
-         // Voter has already voted
-         $now = time();
-         $vote_time = strtotime($previous_vote_time);
-         $minutes_since_last_vote = ($now - $vote_time) / 60;
+    // Check if voting session is open
+    $voting_session_sql = "SELECT * FROM voting_session WHERE status='open'";
+    $voting_session_result = $conn->query($voting_session_sql);
 
-
-         if ($minutes_since_last_vote >= 5) {
-             // 5 minutes have passed, voter can't change vote
-             echo "Sorry, you cannot change your vote as 5 minutes have passed since your last vote.";
-         } else {
-             // Voter can change vote
-             $sql = "UPDATE HAALETUS SET otsus=$vote, hääletuse_aeg=NOW() WHERE id=$voter_id";
-             if ($conn->query($sql) === TRUE) {
-                 echo "Vote changed successfully!";
-             } else {
-                 echo "Error: " . $sql . "<br>" . $conn->error;
-             }
-         }
-     }
+    if ($voting_session_result->num_rows > 0) {
+        // Voting session is open
+        $sql = "UPDATE HAALETUS SET otsus=$vote, hääletuse_aeg=CURRENT_TIMESTAMP WHERE id=$voter_id";
+        if ($conn->query($sql) === TRUE) {
+            echo "Vote registered successfully!";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    } else {
+        // Voting session is closed
+        echo "Sorry, the voting session is closed.";
+    }
 } else {
     // Voter does not exist in the database
     echo "Sorry, you are not registered to vote.";
