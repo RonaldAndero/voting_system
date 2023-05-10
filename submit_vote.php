f2940ea8c0b83cdb156f91490cefb0239dbbcf25
markos <94616705+Paltser@users.noreply.github.com>
2023-05-10 07:20:30 +0300

    Add files via upload
    WIP

--- submit_vote.php
+++ submit_vote.php
@@ -21,30 +21,75 @@ $sql = "SELECT * FROM HAALETUS WHERE eesnimi='$firstname' AND perenimi='$lastnam
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    // Voter exists in the database
-    $row = $result->fetch_assoc();
-    $voter_id = $row['id'];
+  // Voter exists in the database
+  $row = $result->fetch_assoc();
+  $voter_id = $row['id'];
 
-    // Check if voting session is open
-    $voting_session_sql = "SELECT * FROM voting_session WHERE status='open'";
-    $voting_session_result = $conn->query($voting_session_sql);
+  // Check if voting session is open
+  $voting_session_sql = "SELECT * FROM voting_session WHERE status='open'";
+  $voting_session_result = $conn->query($voting_session_sql);
 
-    if ($voting_session_result->num_rows > 0) {
-        // Voting session is open
-        $sql = "UPDATE HAALETUS SET otsus=$vote, hääletuse_aeg=CURRENT_TIMESTAMP WHERE id=$voter_id";
-        if ($conn->query($sql) === TRUE) {
-            echo "Vote registered successfully!";
-        } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-        }
+  if ($voting_session_result->num_rows > 0) {
+    // Voting session is open
+    $sql = "UPDATE HAALETUS SET otsus=$vote, hääletuse_aeg=CURRENT_TIMESTAMP WHERE id=$voter_id";
+    if ($conn->query($sql) === TRUE) {
+      echo "<body style='background-color: #212121;'>
+        <div style='
+          height: 100vh;
+          margin: 0;
+          padding: 0;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          font-family: sans-serif;
+          color: #fff;
+          font-size: 24px;
+        '>Vote registered successfully!</div>";
     } else {
-        // Voting session is closed
-        echo "Sorry, the voting session is closed.";
+      echo "<body style='background-color: #212121;'>
+        <div style='
+          height: 100vh;
+          margin: 0;
+          padding: 0;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          font-family: sans-serif;
+          color: #fff;
+          font-size: 24px;
+        '>Error: " . $sql . "<br>" . $conn->error . "</div>";
     }
+  } else {
+    // Voting session is closed
+    echo "<body style='background-color: #212121;'>
+      <div style='
+        height: 100vh;
+        margin: 0;
+        padding: 0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-family: sans-serif;
+        color: #fff;
+        font-size: 24px;
+      '>Sorry, the voting session is closed.</div>";
+  }
 } else {
-    // Voter does not exist in the database
-    echo "Sorry, you are not registered to vote.";
+  // Voter does not exist in the database
+  echo "<body style='background-color: #212121;'>
+    <div style='
+      height: 100vh;
+      margin: 0;
+      padding: 0;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-family: sans-serif;
+      color: #fff;
+      font-size: 24px;
+    '>Sorry, you are not registered to vote.</div>";
 }
+
 
 $conn->close();
 ?>
